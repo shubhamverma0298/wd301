2836579c64f9080e4a62d2cc81c3cd95fbcf63ec
shubham verma <Shubham@gmail.com>
2024-09-23 13:08:56 +0530

    lesson-2 submit

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -2,20 +2,59 @@ import React from 'react';
 import './TaskCard.css'
 
 interface TaskCard {
-  dueDate: "string",
-  completedAtDate : "string",
-  assigneeName: "string",
+  title: string
+  assigneeName: string
 }
-const TaskCard = (props) =>{
-    return(
-      <div className="TaskItem py-3 px-5">
-        <h4 className="text-xl font-bold">{props.title}</h4>
-        {props.dueDate && <p className='text-left'>Due on: {props.dueDate}</p>}
-        {props.completedAtDate && <p className='text-left'>Completed on: {props.completedAtDate}</p>}
-        <p>AssigneeName : {props.assigneeName}</p>
-      </div>
-    );
-  };
+
+class dueCard implements TaskCard {
+  title: string
+  assigneeName: string
+  dueDate: string
+
+  constructor(title:string, assigneeName:string, dueDate: string){
+      this.title = title
+      this.assigneeName = assigneeName
+      this.dueDate = dueDate
+  }
+}
+
+class completedCard implements TaskCard {
+  title: string
+  assigneeName: string
+  completedAtDate: string
+
+  constructor(title:string, assigneeName:string, completedAtDate: string){
+      this.title = title
+      this.assigneeName = assigneeName
+      this.completedAtDate = completedAtDate
+  }
+}
+
+const TaskCard = (props) => {
+  if (props.dueDate != null){
+      const item = new dueCard(props.title, props.assigneeName, props.dueDate)
+      return (
+          <>
+              <div className='TaskItem mt-3 m-3'>
+                  <h1 className='font-bold text-3xl'>{item.title}</h1>
+                  <p>Due on: {item.dueDate}</p>
+                  <p>Assignee: {item.assigneeName}</p>
+              </div>
+          </>
+      )
+  } else{ 
+      const item = new completedCard(props.title, props.assigneeName, props.completedAtDate)
+      return (
+          <>
+              <div className='TaskItem mt-3 m-3'>
+                  <h1 className='font-bold text-3xl'>{item.title}</h1>
+                  <p>Completed on: {item.completedAtDate}</p>
+                  <p>Assignee: {item.assigneeName}</p>
+              </div>
+          </>
+      )
+  }
+};
 
   export default TaskCard;
   
