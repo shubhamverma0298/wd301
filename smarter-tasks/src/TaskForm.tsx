2f77dc79590eb3494190257cf3ddb9dc5a9fc36b
shubham verma <Shubham@gmail.com>
2024-09-25 14:06:45 +0530

    l-3 rs-2

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -25,6 +25,9 @@ addTask: React.FormEventHandler<HTMLFormElement> = (event) => {
         description:this.state.description,
         DueDate:this.state.DueDate,
       };
+    if(newTask.title.length === 0 || newTask.DueDate.length === 0){
+      return;
+    }
       this.props.addTask(newTask);
       this.setState({ title: "" });
       this.setState({description:""});
@@ -44,15 +47,21 @@ DueDateChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
 };
  render(){
     return (
-     <div>Task form
+     <div  className="TaskItem ">
         <form onSubmit={this.addTask}>
-            <label className="TaskItem" htmlFor="todoTitle">Title :</label>
-            <input className="TaskItem border border-grey-300 rounded text-grey-900 w-full p-2 text-sm " id="todoTitle" type="text" value={this.state.title} onChange={this.titleChanged} required/>
+            <div>
+            <label className="TaskItem " htmlFor="todoTitle">Title :</label>
+            <input className="TaskItem border border-grey-300 rounded text-grey-900 w-full p-2 text-sm " id="todoTitle" type="text" value={this.state.title} onChange={this.titleChanged} />
+            </div>
+            <div>
             <label className="TaskItem" htmlFor="todoDescription">Description :</label>
-            <input className="TaskItem border border-grey-300 rounded text-grey-900 w-full p-2 text-sm" id="todoDescription" type="text" value={this.state.description} onChange={(this.descriptionChanged)} required/>
+            <input className="TaskItem border border-grey-300 rounded text-grey-900 w-full p-2 text-sm mp-2" id="todoDescription" type="text" value={this.state.description} onChange={(this.descriptionChanged)} />
+            </div>
+            <div>
             <label className="TaskItem" htmlFor="todoDueDate">DueDate :</label>
-            <input className="TaskItem border border-grey-300 rounded text-grey-900 w-full p-2 text-sm" id="todoDueDate" type="text" value={this.state.DueDate} onChange={(this.DueDateChanged)} required/>
-            <button  className="TaskItem border border-grey-300 rounded p-2 text-sm" id="addTaskButton" type="submit">Add item</button>
+            <input className="TaskItem border border-grey-300 rounded text-grey-900 w-full p-2 text-sm" id="todoDueDate" type="text" value={this.state.DueDate} onChange={(this.DueDateChanged)} />
+            </div>
+            <button  className="TaskItem border border-grey-300 rounded p-2 text-sm bg-green-500" id="addTaskButton" type="submit">Add item</button>
         </form>
      </div>
     )
