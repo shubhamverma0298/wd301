c3a7093e1f04b2fae2799877cac7f93e7196a4a0
shubham verma <Shubham@gmail.com>
2024-09-30 13:01:16 +0530

    l-7 usereducer added

--- smarter-tasks/src/pages/projects/index.tsx
+++ smarter-tasks/src/pages/projects/index.tsx
@@ -1,6 +1,14 @@
+import ProjectList from "./ProjectList";
+
+
+
 const Projects = () => {
-    return (
-      <h2>Projects</h2>
+  
+  return (
+    <>
+    <h2>Projects</h2>
+    <ProjectList />
+    </>
     )
   }
   export default Projects;
