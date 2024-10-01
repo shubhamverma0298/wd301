e28ba3a26eb50c053c38b7b3f7fb72bd42485281
shubham verma <Shubham@gmail.com>
2024-10-01 15:59:16 +0530

    l-7

--- smarter-tasks/src/pages/members/MemberListItems.tsx
+++ smarter-tasks/src/pages/members/MemberListItems.tsx
@@ -17,7 +17,7 @@ export default function MemberListItems() {
   return (
     <>
       {members.map((member: any) => (
-        <div key={member.id} className="member text-left block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <div key={member.id} className="text-left block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 member">
           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">Name:  {member.name}</h5>
           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">Email: {member.email}</h5>
           <button
