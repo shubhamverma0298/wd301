0c376c8093893ea6e7e57419deb96421db20009a
shubham verma <Shubham@gmail.com>
2024-10-04 12:18:41 +0530

    l-9 index.tsx added

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h3rsvt4e7ao"
+    "revision": "0.1bhvfani74o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
